a517edd6e91e394003f851fd4c6a9644dba32945
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-03-05 21:47:13 +0100

    Remove useless enterprise modules

--- src/enterprise_modules/enterprise.module.ts
+++ src/enterprise_modules/enterprise.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ChatModule } from './chat/chat.module';
-import { GraphModule } from './graphs/graphs.module';
-import { SampleModule } from './samples/samples.module';
 
 @Module({
-    imports: [
-        SampleModule,
-        GraphModule,
-        ChatModule
-    ],
+    imports: [],
     providers: [],
     controllers: []
 })
