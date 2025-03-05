364457ac3b0bcae687dad7018e5edd2367eec739
Cédric HERZOG <cedric.herzpg@codeclarity.io>
2025-03-05 08:58:47 +0100

    Clean license module

--- src/codeclarity_modules/results/licenses/licenses.module.ts
+++ src/codeclarity_modules/results/licenses/licenses.module.ts
@@ -6,19 +6,18 @@ import { Result } from 'src/entity/codeclarity/Result';
 import { AnalysisResultsService } from '../results.service';
 import { LicenseRepository } from 'src/codeclarity_modules/knowledge/LicenseRepository';
 import { Package } from 'src/entity/knowledge/Package';
-import { ProjectMemberService } from 'src/base_modules/projects/projectMember.service';
-import { AnalysesMemberService } from 'src/base_modules/analyses/analysesMembership.service';
 import { License } from 'src/entity/knowledge/License';
-import { OrganizationMemberships } from 'src/base_modules/organizations/organization.memberships.entity';
-import { Project } from 'src/entity/codeclarity/Project';
-import { Analysis } from 'src/entity/codeclarity/Analysis';
 import { OrganizationsModule } from 'src/base_modules/organizations/organizations.module';
+import { ProjectsModule } from 'src/base_modules/projects/projects.module';
+import { AnalysesModule } from 'src/base_modules/analyses/analyses.module';
 
 @Module({
     imports: [
         OrganizationsModule,
+        ProjectsModule,
+        AnalysesModule,
         TypeOrmModule.forFeature(
-            [Result, OrganizationMemberships, Project, Analysis],
+            [Result],
             'codeclarity'
         ),
         TypeOrmModule.forFeature([Package, License], 'knowledge')
@@ -27,8 +26,6 @@ import { OrganizationsModule } from 'src/base_modules/organizations/organization
         LicensesService,
         AnalysisResultsService,
         LicenseRepository,
-        ProjectMemberService,
-        AnalysesMemberService
     ],
     controllers: [LicensesController]
 })
