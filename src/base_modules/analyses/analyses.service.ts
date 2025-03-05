74ef481051c3f7ffd3b0f88d58118aceab31dda7
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-03-05 19:48:22 +0100

    Comment analysis service

--- src/base_modules/analyses/analyses.service.ts
+++ src/base_modules/analyses/analyses.service.ts
@@ -47,11 +47,11 @@ export class AnalysesService {
      * @throws {NotAuthorized} In case the user is not allowed to perform the action on the org
      * @throws {AnalyzerDoesNotExist} In case the referenced analyzer does not exist on the org
      * @throws {AnaylzerMissingConfigAttribute} In case config options required by the anylzer were not provided
-     * @param orgId The id of the organizaiton to which the project belongs
+     * @param orgId The id of the organization to which the project belongs
      * @param projectId The id of the project on which the analysis should be performed
      * @param analysisData The analysis create body supplied by the user
      * @param user The authenticated user
-     * @returns
+     * @returns The ID of the created analysis
      */
     async create(
         orgId: string,
@@ -59,26 +59,41 @@ export class AnalysesService {
         analysisData: AnalysisCreateBody,
         user: AuthenticatedUser
     ): Promise<string> {
-        // (1) Check if user has access to org
+        // Check if the user has the required role to perform actions on the organization
         await this.organizationsRepository.hasRequiredRole(orgId, user.userId, MemberRole.USER);
 
-        // (2) Check if the project belongs to the org
+        // Verify that the project belongs to the specified organization
         await this.projectMemberService.doesProjectBelongToOrg(projectId, orgId);
 
-        const analyzer = await this.analyzersRepository.getAnalyzerById(analysisData.analyzer_id)
+        // Retrieve the analyzer details based on the provided analyzer ID
+        const analyzer = await this.analyzersRepository.getAnalyzerById(analysisData.analyzer_id);
 
-        const project = await this.projectsRepository.getProjectById(projectId)
+        // Fetch the project details using the project ID
+        const project = await this.projectsRepository.getProjectById(projectId);
 
-        const creator = await this.usersRepository.getUserById(user.userId)
+        // Get the user details of the creator of the analysis
+        const creator = await this.usersRepository.getUserById(user.userId);
 
-        const organization = await this.organizationsRepository.getOrganizationById(orgId)
+        // Retrieve organization details based on the organization ID
+        const organization = await this.organizationsRepository.getOrganizationById(orgId);
 
+        // Initialize an object to hold the configuration structure for the analyzer steps
         const config_structure: { [key: string]: any } = {};
+
+        // Initialize an object to hold the final configuration provided by the user
         const config: { [key: string]: any } = {};
+
+        // Array to store stages of the analysis process
         const stages: AnalysisStage[][] = [];
+
+        // Iterate through each stage defined in the analyzer
         for (const stage of analyzer.steps) {
+            // Initialize an array to hold steps within a stage
             const steps: AnalysisStage[] = [];
+
+            // Iterate through each step within the current stage
             for (const step of stage) {
+                // Define the initial state of each step with default values
                 steps.push({
                     name: step.name,
                     version: step.version,
@@ -86,6 +101,8 @@ export class AnalysesService {
                     result: undefined,
                     config: {}
                 });
+
+                // If the step requires configuration, add it to the config_structure object
                 if (step.config) {
                     for (const [key, value] of Object.entries(step.config)) {
                         if (!config_structure[step.name]) config_structure[step.name] = {};
@@ -93,10 +110,12 @@ export class AnalysesService {
                     }
                 }
             }
+
+            // Add the steps array to the stages array
             stages.push(steps);
         }
 
-        // Provider attributes overwrite persistant config attributes
+        // Merge user-provided configuration with default or existing configuration
         for (const [pluginName, _] of Object.entries(analysisData.config)) {
             for (const [key, value] of Object.entries(_)) {
                 if (!config[pluginName]) config[pluginName] = {};
@@ -104,6 +123,7 @@ export class AnalysesService {
             }
         }
 
+        // Validate the configuration provided by the user against the required attributes
         for (const [pluginName, plugin_config] of Object.entries(config_structure)) {
             for (const [key] of Object.entries(plugin_config)) {
                 const config_element = config_structure[pluginName][key];
@@ -113,6 +133,7 @@ export class AnalysesService {
             }
         }
 
+        // Create a new analysis object with the provided and default values
         const analysis = new Analysis();
         analysis.status = AnalysisStatus.REQUESTED;
         analysis.stage = 0;
@@ -128,9 +149,10 @@ export class AnalysesService {
         analysis.organization = organization;
         analysis.integration = project.integration;
 
+        // Save the newly created analysis to the database
         const created_analysis = await this.analysesRepository.saveAnalysis(analysis);
 
-        // Send message to aqmp to start the anaylsis
+        // Prepare to send a message to RabbitMQ to start the analysis process
         const queue = this.configService.getOrThrow<string>('AMQP_ANALYSES_QUEUE');
         const amqpHost = `${this.configService.getOrThrow<string>(
             'AMQP_PROTOCOL'
@@ -140,38 +162,55 @@ export class AnalysesService {
             )}:${this.configService.getOrThrow<string>('AMQP_PORT')}`;
 
         try {
+            // Connect to RabbitMQ using the configured settings
             const conn = await amqp.connect(amqpHost);
             const ch1 = await conn.createChannel();
             await ch1.assertQueue(queue);
 
-            // A project can be analyzed with or without an integration
-            // This is the user has uploaded files
+            // Determine the integration ID if the project has an associated integration
             let integration_id = null;
             if (project.integration) {
                 integration_id = project.integration.id;
             }
+
+            // Create the message payload to start the analysis process
             const message: AnalysisStartMessageCreate = {
                 analysis_id: created_analysis.id,
                 integration_id: integration_id,
                 organization_id: orgId
             };
+
+            // Send the message to RabbitMQ queue
             ch1.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+
+            // Close the channel after sending the message
             await ch1.close();
         } catch (err) {
+            // Throw an error if there is a problem with connecting or messaging via RabbitMQ
             throw new RabbitMQError(err);
         }
 
+        // Return the ID of the created analysis as a confirmation
         return created_analysis.id;
     }
 
     /**
+     * Retrieve a specific analysis by its ID.
+     *
+     * This function performs several checks to ensure that the user is authorized to access the requested analysis:
+     * 1. Checks if the user has the required role within the specified organization.
+     * 2. Verifies that the project belongs to the specified organization.
+     * 3. Confirms that the analysis belongs to the specified project.
+     *
+     * If any of these checks fail, appropriate exceptions are thrown.
+     *
      * @throws {NotAuthorized} In case the user is not allowed to perform the action on the org
-     * @throws {EntityNotFound} In case the project does not exist
-     * @param orgId
-     * @param projectId
-     * @param id
-     * @param user
-     * @returns
+     * @throws {EntityNotFound} In case the project does not exist or the analysis does not belong to the project
+     * @param orgId The ID of the organization to which the project and analysis belong.
+     * @param projectId The ID of the project associated with the analysis.
+     * @param id The ID of the analysis to retrieve.
+     * @param user The authenticated user making the request.
+     * @returns The requested Analysis object.
      */
     async get(
         orgId: string,
@@ -185,7 +224,7 @@ export class AnalysesService {
         // (2) Check if the project belongs to the org
         await this.projectMemberService.doesProjectBelongToOrg(projectId, orgId);
 
-        // (3) Check if the analyses belongs to the project
+        // (3) Check if the analysis belongs to the project
         await this.analysesRepository.doesAnalysesBelongToProject(id, projectId);
 
         const analysis = await this.analysesRepository.getAnalysisById(id)
@@ -194,13 +233,22 @@ export class AnalysesService {
     }
 
     /**
+     * Retrieve chart data for a specific analysis.
+     *
+     * This function performs several checks to ensure that the user is authorized to access the requested analysis:
+     * 1. Checks if the user has the required role within the specified organization.
+     * 2. Verifies that the project belongs to the specified organization.
+     * 3. Confirms that the analysis belongs to the specified project.
+     *
+     * If any of these checks fail, appropriate exceptions are thrown.
+     *
      * @throws {NotAuthorized} In case the user is not allowed to perform the action on the org
-     * @throws {EntityNotFound} In case the project does not exist
-     * @param orgId
-     * @param projectId
-     * @param id
-     * @param user
-     * @returns
+     * @throws {EntityNotFound} In case the project does not exist or the analysis does not belong to the project
+     * @param orgId The ID of the organization to which the project and analysis belong.
+     * @param projectId The ID of the project associated with the analysis.
+     * @param id The ID of the analysis for which to retrieve chart data.
+     * @param user The authenticated user making the request.
+     * @returns An array of objects representing the chart data.
      */
     async getChart(
         orgId: string,
@@ -214,14 +262,19 @@ export class AnalysesService {
         // (2) Check if the project belongs to the org
         await this.projectMemberService.doesProjectBelongToOrg(projectId, orgId);
 
-        // (3) Check if the analyses belongs to the project
+        // (3) Check if the analysis belongs to the project
         await this.analysesRepository.doesAnalysesBelongToProject(id, projectId);
 
-        // const patchesOutput: PatchesOutput = await getPatchingResult(db, id);
+        // Fetch SBOM output for the specified analysis ID
         const sbomOutput: SbomOutput = await this.sbomRepository.getSbomResult(id);
+
+        // Fetch vulnerabilities output for the specified analysis ID
         const vulnOutput: VulnsOuptut = await this.findingsRepository.getVulnsResult(id);
+
+        // Fetch licenses output for the specified analysis ID
         const licensesOutput: LicensesOutput = await this.licensesRepository.getLicensesResult(id);
 
+        // Construct and return the chart data array
         return [
             {
                 x: 'Latest',
@@ -261,12 +314,20 @@ export class AnalysesService {
     }
 
     /**
+     * Retrieve multiple analyses for a project with pagination.
+     *
+     * This function performs several checks to ensure that the user is authorized to access the requested analyses:
+     * 1. Checks if the user has the required role within the specified organization.
+     * 2. Verifies that the project belongs to the specified organization.
+     *
+     * If any of these checks fail, appropriate exceptions are thrown.
+     *
      * @throws {NotAuthorized} In case the user is not allowed to perform the action on the org
-     * @param organizationId
-     * @param projectId
-     * @param paginationUserSuppliedConf
-     * @param user
-     * @returns
+     * @param organizationId The ID of the organization to which the project and analyses belong.
+     * @param projectId The ID of the project associated with the analyses.
+     * @param paginationUserSuppliedConf Pagination configuration provided by the user, including entries per page and current page.
+     * @param user The authenticated user making the request.
+     * @returns A paginated list of Analysis objects.
      */
     async getMany(
         organizationId: string,
@@ -274,7 +335,7 @@ export class AnalysesService {
         paginationUserSuppliedConf: PaginationUserSuppliedConf,
         user: AuthenticatedUser
     ): Promise<TypedPaginatedData<Analysis>> {
-        // (1) Check if the user is allowed to create a analyzer (is atleast user)
+        // (1) Check if the user is allowed to create an analyzer (is at least a user)
         await this.organizationsRepository.hasRequiredRole(
             organizationId,
             user.userId,
@@ -304,16 +365,25 @@ export class AnalysesService {
         if (paginationUserSuppliedConf.currentPage)
             currentPage = Math.max(0, paginationUserSuppliedConf.currentPage);
 
-        return this.analysesRepository.getAnalysisByProjectId(projectId, currentPage, entriesPerPage)
+        return this.analysesRepository.getAnalysisByProjectId(projectId, currentPage, entriesPerPage);
     }
 
     /**
+     * Delete a specific analysis by its ID.
+     *
+     * This function performs several checks to ensure that the user is authorized to delete the requested analysis:
+     * 1. Checks if the user has the required role within the specified organization.
+     * 2. Verifies that the project belongs to the specified organization.
+     * 3. Confirms that the analysis belongs to the specified project.
+     *
+     * If any of these checks fail, appropriate exceptions are thrown.
+     *
      * @throws {NotAuthorized} In case the user is not allowed to perform the action on the org
-     * @throws {EntityNotFound} In case the project does not exist
-     * @param orgId
-     * @param projectId
-     * @param id
-     * @param user
+     * @throws {EntityNotFound} In case the project does not exist or the analysis does not belong to the project
+     * @param orgId The ID of the organization to which the project and analysis belong.
+     * @param projectId The ID of the project associated with the analysis.
+     * @param id The ID of the analysis to delete.
+     * @param user The authenticated user making the request.
      */
     async delete(
         orgId: string,
@@ -327,10 +397,10 @@ export class AnalysesService {
         // (2) Check if the project belongs to the org
         await this.projectMemberService.doesProjectBelongToOrg(projectId, orgId);
 
-        // (3) Check if the analyses belongs to the project
+        // (3) Check if the analysis belongs to the project
         await this.analysesRepository.doesAnalysesBelongToProject(id, projectId);
 
-        const analysis = await this.analysesRepository.getAnalysisById(id, { results: true })
+        const analysis = await this.analysesRepository.getAnalysisById(id, { results: true });
 
         for (const result of analysis.results) {
             await this.resultsRepository.delete(result.id);
