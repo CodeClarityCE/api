def9a6002fa9c717b5a66f83a4b1d6da7d69cc0b
Cédric HERZOG <cedric.herzpg@codeclarity.io>
2025-03-05 08:57:40 +0100

    Clean patching module

--- src/codeclarity_modules/results/patching/patching.module.ts
+++ src/codeclarity_modules/results/patching/patching.module.ts
@@ -4,27 +4,22 @@ import { PatchingController } from './patching.controller';
 import { AnalysisResultsService } from '../results.service';
 import { Result } from 'src/entity/codeclarity/Result';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProjectMemberService } from 'src/base_modules/projects/projectMember.service';
-import { AnalysesMemberService } from 'src/base_modules/analyses/analysesMembership.service';
-import { Project } from 'src/entity/codeclarity/Project';
-import { Analysis } from 'src/entity/codeclarity/Analysis';
-import { Invitation } from 'src/entity/codeclarity/Invitation';
-import { EmailService } from 'src/base_modules/email/email.service';
-import { Email } from 'src/entity/codeclarity/Email';
 import { UsersModule } from 'src/base_modules/users/users.module';
 import { OrganizationsModule } from 'src/base_modules/organizations/organizations.module';
+import { AnalysesModule } from 'src/base_modules/analyses/analyses.module';
+import { ProjectsModule } from 'src/base_modules/projects/projects.module';
+import { EmailModule } from 'src/base_modules/email/email.module';
 
 @Module({
     imports: [
         UsersModule,
         OrganizationsModule,
+        AnalysesModule,
+        ProjectsModule,
+        EmailModule,
         TypeOrmModule.forFeature(
             [
-                Result,
-                Project,
-                Analysis,
-                Invitation,
-                Email
+                Result
             ],
             'codeclarity'
         )
@@ -32,9 +27,6 @@ import { OrganizationsModule } from 'src/base_modules/organizations/organization
     providers: [
         PatchingService,
         AnalysisResultsService,
-        ProjectMemberService,
-        AnalysesMemberService,
-        EmailService
     ],
     controllers: [PatchingController]
 })
