62ee7f9e9f2e59c73ca8624ed6bfefd5a1e50992
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-17 12:35:46 +0200

    fix(datasource): correct entity path for plugins in DataSource configuration

--- src/datasources/plugins.datasource.ts
+++ src/datasources/plugins.datasource.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 import { buildBaseOptions } from './base-options';
 
 const isTs = __filename.endsWith('.ts');
-const entities = [isTs ? 'src/**/*.entity.ts' : 'dist/src/**/*.entity.js'];
+const entities = [isTs ? 'src/base_modules/plugins/plugin.entity.ts' : 'dist/src/base_modules/plugins/plugin.entity.js'];
 const migrations = [isTs ? 'src/migrations/plugins/*.ts' : 'dist/src/migrations/plugins/*.js'];
 
 export const PluginsDataSource = new DataSource({
