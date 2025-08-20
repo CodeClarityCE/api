542b27a03e210ea0735d74381308122778edfd42
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-20 16:31:42 +0200

    feat(migrations): add indexes for improved PHP vulnerability detection performance

--- src/migrations/plugins/1754648240560-Init.ts
+++ src/migrations/plugins/1754648240560-Init.ts
@@ -6,7 +6,7 @@ export class Init1754648240560 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         // Create plugin table
         await queryRunner.query(
-            `CREATE TABLE "plugin" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying, "version" character varying, "description" character varying, "depends_on" jsonb, "config" jsonb, CONSTRAINT "PK_26c98f1bfcce9906f8bb0b11e5b" PRIMARY KEY ("id"))`
+            `CREATE TABLE IF NOT EXISTS "plugin" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying, "version" character varying, "description" character varying, "depends_on" jsonb, "config" jsonb, CONSTRAINT "PK_26c98f1bfcce9906f8bb0b11e5b" PRIMARY KEY ("id"))`
         );
     }
 
