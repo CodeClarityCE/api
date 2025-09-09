97747b51442611a9772d729222697eb94df527e9
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-09-09 10:27:06 +0200

    feat: add NonAuthEndpoint decorator to metrics retrieval endpoint

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 import { MetricsService } from './metrics.service';
+import { NonAuthEndpoint } from '../decorators/SkipAuthDecorator';
 
 @Controller('metrics')
 export class MetricsController {
     constructor(private readonly metricsService: MetricsService) {}
 
     @Get()
+    @NonAuthEndpoint()
     async getMetrics(@Res() res: FastifyReply) {
         const metrics = await this.metricsService.getMetrics();
         res.type('text/plain');
